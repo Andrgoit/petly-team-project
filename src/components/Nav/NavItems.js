9cbb677518c030ab833ee323529d2ffe3c6009a1
Andrgoit <stakgrom@gmail.com>
2023-01-31 23:43:37 +0200

    добавил временно ссылку на Юзера

--- src/components/Nav/NavItems.js
+++ src/components/Nav/NavItems.js
@@ -2,6 +2,7 @@ const items = [
   { text: 'News', href: '/news' },
   { text: 'Find pet', href: '/notices' },
   { text: 'Our friends', href: '/friends' },
+  { text: 'User', href: '/user' },
 ];
 
 export default items;
