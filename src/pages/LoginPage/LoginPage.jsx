ffbd127f6751ec11dc03be53b58931257d7d4e13
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2023-02-01 17:36:48 +0200

    login-without-problem

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import LoginForm from '../../components/AuthForm/LoginForm/LoginForm';
 
-import { AuthContainer, Title } from './LoginPage.styled';
-
 const Login = () => {
   // RTK Query (Отримання "токену")
   // const { token } = useSelector(getUser);
@@ -15,6 +13,3 @@ const Login = () => {
 };
 
 export default Login;
-//  <AuthContainer>
-//    <Title>Login</Title>
-//  </AuthContainer>;
