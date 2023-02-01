3ea639efee2f36e4a722519a523240946f67fbba
pavliv182 <uptn182@gmail.com>
2023-02-01 21:37:07 +0200

    add proptypes

--- src/components/News/NewsList/NewsList.jsx
+++ src/components/News/NewsList/NewsList.jsx
@@ -1,8 +1,11 @@
+import PropTypes from 'prop-types';
+
 import { List } from './NewsList.styled';
 
 import NewsItem from '../NewsItem';
 
-function NewsList({ data }) {
+function NewsList({ data = [] }) {
+  console.log(data);
   const elements = data.map(({ _id, date, text, title, url }) => (
     <NewsItem key={_id} date={date} text={text} title={title} url={url} />
   ));
@@ -11,3 +14,19 @@ function NewsList({ data }) {
 }
 
 export default NewsList;
+
+NewsList.defaultProps = {
+  data: [],
+};
+
+NewsList.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ),
+};
