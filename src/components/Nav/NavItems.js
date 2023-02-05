2771c84ac1965f5f3212e6f9ac99746af590c280
Andrey Hlushchenko <93829515+Andrgoit@users.noreply.github.com>
2023-02-05 22:43:25 +0200

    Update NavItems.js

--- src/components/Nav/NavItems.js
+++ src/components/Nav/NavItems.js
@@ -2,7 +2,6 @@ const items = [
   { text: 'News', href: '/news' },
   { text: 'Find pet', href: '/notices' },
   { text: 'Our friends', href: '/friends' },
-  { text: 'User', href: '/user' },
 ];
 
 export default items;
