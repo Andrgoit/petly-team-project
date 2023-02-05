cc3b3d1705cbf65a32a4d1b6315bfe124604c208
Andrgoit <stakgrom@gmail.com>
2023-02-05 03:34:44 +0200

    fix

--- src/components/PetsList/PetsList.jsx
+++ src/components/PetsList/PetsList.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import PetsItem from 'components/PetsItem/PetsItem';
 
 export default function PetsList({ pets }) {
-  const elements = pets.map(({ avatar, name, date, breed, comments }) => {
-    return (
-      <PetsItem
-        key={PetsItem}
-        name={name}
-        avatar={avatar}
-        date={date}
-        breed={breed}
-        comments={comments}
-      />
+  if (pets) {
+    const elements = pets.map(
+      ({ _id, avatar, name, date, breed, comments }) => {
+        return (
+          <PetsItem
+            key={_id}
+            name={name}
+            avatar={avatar}
+            date={date}
+            breed={breed}
+            comments={comments}
+          />
+        );
+      }
     );
-  });
-  return <div>{elements}</div>;
+    return <div>{elements}</div>;
+  }
 }
